cd2e3f81c207124d74814fc0ea0d9cf0ef24b332
Tasty <no@email>
2021-09-04 21:07:28 +0100

    core mechanic done

--- SRC/GameOverMind.cs
+++ SRC/GameOverMind.cs
@@ -7,6 +7,16 @@ public class GameOverMind : Control {
     private TextureButton button;
 
     private Label honey_count;
+    private Label persec_count;
+
+    private float persec_calc() {
+        float rtn = 0;
+        foreach(UpdateBarBasic n in GetTree().GetNodesInGroup("Timer")) {
+            rtn += n.persec;
+        }
+
+        return rtn;
+    }
 
     private void button_pressed() {
         honey += 1;
@@ -17,9 +27,11 @@ public class GameOverMind : Control {
         button.Connect("pressed", this, nameof(button_pressed));
 
         honey_count = GetNode<Label>("menu_stack/click_pain/vbox/click_info_case/vbox/unit_count");
+        persec_count = GetNode<Label>("menu_stack/click_pain/vbox/click_info_case/vbox/unit_per_sec");
     }
 
     public override void _Process(float delta) {
         honey_count.Text = honey + " Drams of Honey";
+        persec_count.Text = persec_calc() + " Drams per second";
     }
 }
--- SRC/UpdateBarBasic.cs
+++ SRC/UpdateBarBasic.cs
@@ -10,6 +10,7 @@ public class UpdateBarBasic : Panel {
     [Export] float cycle_time = 10f;
     [Export] float cycle_profit = 1f;
     [Export] float unlock_cost = 0;
+    [Export] int max_count = 100;
 
     public int count = 0;
 
@@ -17,6 +18,7 @@ public class UpdateBarBasic : Panel {
 
     private Button button;
     private Label count_text;
+    private Timer timer;
 
     //TODO this is linear scalling I want it to be exponetual at some point
     public float cost {
@@ -25,25 +27,50 @@ public class UpdateBarBasic : Panel {
         }
     }
 
+    public float persec {
+        get {
+            return (cycle_profit / cycle_time) * count;
+        }
+    }
+
     private void update_tooptip() {
         button.HintTooltip = "add one " + type + "\ncost: " + cost;
     }
 
-    //FIXME not checking or debitting honey, just incromenting.
     private void button_pressed() {
-        count += 1;
-        update_tooptip();
+        if (count < 100) {
+            if (overmind.honey >= cost) {
+                overmind.honey -= cost;
+                count += 1;
+                update_tooptip();
+            }
+            
+        } else {
+            UpdateBarBasic dupe = this.Duplicate() as UpdateBarBasic;
+            dupe.count = 0;
+
+            GetParent().AddChildBelowNode(this, dupe);
+            button.Visible = false;
+        }
+    }
+
+    private void timer_tick() {
+        overmind.honey += cycle_profit * count;
     }
 
     public override void _Ready() {
         overmind = GetTree().Root.GetNode<GameOverMind>("Control");
 
+        count_text = GetNode<Label>("Label");
+
         button = GetNode<Button>("Button");
         button.Connect("pressed", this, nameof(button_pressed));
-        count_text = GetNode<Label>("Label");
-        
-        Visible = false;
-        
+
+        timer = GetNode<Timer>("Timer");
+        timer.Connect("timeout", this, nameof(timer_tick));
+        timer.Start(cycle_time);
+
+        Visible = false;        
         update_tooptip();
     }
 
